873075b259fbb46271a05fcd4ec71170034009e7
sendistephen <sendi.stev@gmail.com>
2023-11-23 18:08:45 +0300

    Implement client-side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,12 @@
 'use client';
 
-import { Button, Callout, TextField, TextFieldInput } from '@radix-ui/themes';
+import {
+  Button,
+  Callout,
+  Text,
+  TextField,
+  TextFieldInput,
+} from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import { Controller, useForm } from 'react-hook-form';
@@ -8,15 +14,25 @@ import { useRouter } from 'next/navigation';
 import axios from 'axios';
 import { useState } from 'react';
 import { InfoCircledIcon } from '@radix-ui/react-icons';
-interface IssueForm {
-  title: string;
-  description: string;
-}
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { createIssueSchema } from '@/app/validationSchema';
+
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 function NewIssuePage() {
   const [error, setError] = useState('');
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+
   const router = useRouter();
+
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
 
   return (
     <div className='max-w-xl'>
@@ -42,6 +58,11 @@ function NewIssuePage() {
         <TextField.Root>
           <TextFieldInput placeholder='Title' {...register('title')} />
         </TextField.Root>
+        {errors.title && (
+          <Text color='red' as='p'>
+            {errors.title.message}
+          </Text>
+        )}
 
         <Controller
           name='description'
@@ -50,6 +71,12 @@ function NewIssuePage() {
             <SimpleMDE placeholder='Description' {...field} />
           )}
         />
+        {errors.description && (
+          <Text color='red' as='p'>
+            {errors.description.message}
+          </Text>
+        )}
+
         <Button>Submit New Issue</Button>
       </form>
     </div>
