e1799ab2ea3356013f798163e0fa2b974379e518
sendistephen <sendi.stev@gmail.com>
2023-11-26 21:51:32 +0300

    Fix cache issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -41,6 +41,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       setSubmitting(true);
       if (issue) await axios.patch(`/api/issues/${issue?.id}`, data);
       else await axios.post('/api/issues', data);
+      router.refresh();
 
       router.push('/issues');
     } catch (error) {
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -52,4 +52,7 @@ const IssuesPage = async () => {
     </div>
   );
 };
+
+export const dynamic = 'force-dynamic';
+
 export default IssuesPage;
