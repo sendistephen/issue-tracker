c3aa9af20c628b9478453e958bd96b0817c9b0d4
sendistephen <sendi.stev@gmail.com>
2023-11-26 19:13:22 +0300

    Update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -7,6 +7,7 @@ import { Issue } from '@prisma/client';
 import { InfoCircledIcon } from '@radix-ui/react-icons';
 import { Button, Callout, TextField, TextFieldInput } from '@radix-ui/themes';
 import axios from 'axios';
+import classNames from 'classnames';
 import 'easymde/dist/easymde.min.css';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
@@ -34,7 +35,9 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setSubmitting(true);
-      await axios.post('/api/issues', data);
+      if (issue) await axios.patch(`/api/issues/${issue?.id}`, data);
+      else await axios.post('/api/issues', data);
+
       router.push('/issues');
     } catch (error) {
       setError('An expected error occured.');
@@ -74,7 +77,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         )}
 
         <Button disabled={isSubmitting}>
-          Submit New Issue {isSubmitting && <Spinner />}
+          {issue ? 'Update Issue' : 'Submit New Issue'}{' '}
+          {isSubmitting && <Spinner />}
         </Button>
       </form>
     </div>
