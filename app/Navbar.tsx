0fe424bbd8f1b2615ba985a9bc35e5666e0425fa
sendistephen <sendi.stev@gmail.com>
2023-12-04 22:29:52 +0300

    Add the login and logout links

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -4,9 +4,12 @@ import React from 'react';
 import { AiFillBug } from 'react-icons/ai';
 import classnames from 'classnames';
 import { usePathname } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+import { Box } from '@radix-ui/themes';
 
 function Navbar() {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
 
   const links = [
     { label: 'Dashboard', href: '/' },
@@ -20,19 +23,28 @@ function Navbar() {
       </Link>
       <ul className='flex space-x-6'>
         {links.map((link) => (
-          <Link
-            key={link.href}
-            href={link.href}
-            className={classnames({
-              'text-zinc-800': link.href === currentPath,
-              'text-zinc-500': link.href !== currentPath,
-              'hover:text-zinc-800 transition-colors': true,
-            })}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className={classnames({
+                'text-zinc-800': link.href === currentPath,
+                'text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors': true,
+              })}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+      <Box>
+        {status === 'authenticated' && (
+          <Link href='/api/auth/signout'>Sign out</Link>
+        )}
+        {status === 'unauthenticated' && (
+          <Link href='/api/auth/signin'>Sign in</Link>
+        )}
+      </Box>
     </nav>
   );
 }
---
+++ app/auth/Provider.tsx
@@ -0,0 +1,9 @@
+'use client';
+import { SessionProvider } from 'next-auth/react';
+import React, { PropsWithChildren } from 'react';
+
+function AuthProvider({ children }: PropsWithChildren) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
+
+export default AuthProvider;
