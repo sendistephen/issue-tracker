e8a920b6a69b8a43f1cb8cd5936288b505927455
sendistephen <sendi.stev@gmail.com>
2023-12-16 19:12:40 +0300

    Fix filtering rows

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -2,7 +2,7 @@
 
 import { Status } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 type StatusOption = {
   label: string;
@@ -17,11 +17,17 @@ const status: StatusOption[] = [
 
 const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
     <Select.Root
+      defaultValue={searchParams.get('status') || ''}
       onValueChange={(status) => {
-        const query = status ? `?status=${status}` : '';
+        const params = new URLSearchParams();
+        if (status) params.append('status', status);
+        if (searchParams.get('orderBy'))
+          params.append('orderBy', searchParams.get('orderBy')!);
+        const query = params.size ? '?' + params.toString() : '';
         router.push('/issues/list' + query);
       }}
     >
