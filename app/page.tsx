55fbd26605cf45a5dc1b30f0b34754a1072c8ad9
sendistephen <sendi.stev@gmail.com>
2023-12-17 22:15:25 +0300

    Build the latest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,44 @@
+import prisma from '@/prisma/client';
 import Pagination from './components/Pagination';
+import { Avatar, Card, Flex, Heading, Table } from '@radix-ui/themes';
+import Link from 'next/link';
+import { IssueStatusBadge } from './components';
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
+export default async function Home() {
+  const issues = await prisma.issue.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+    take: 5,
+    include: { assignedToUser: true }, // eager loading technique
+  });
   return (
-    <div>
-      <Pagination
-        currentPage={parseInt(searchParams.page)}
-        pageSize={5}
-        itemCount={20}
-      />
-    </div>
+    <Card>
+      <Heading>Latest Issues</Heading>
+      <Table.Root>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                <Flex justify='between' align='center'>
+                  <Flex direction='column' align='start' gap='2'>
+                    <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                    <IssueStatusBadge status={issue.status} />
+                  </Flex>
+                  {issue.assignedToUserId && (
+                    <Avatar
+                      src={issue.assignedToUser?.image!}
+                      fallback='?'
+                      radius='full'
+                      size='2'
+                    />
+                  )}
+                </Flex>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </Card>
   );
 }
